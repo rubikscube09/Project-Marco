aae0b0f2f23c88bb1745fd1033975cd013fd01b4
haozhu <haozhu@uchicago.edu>
2020-02-12 17:07:54 -0600

    City to Country

---
+++ city_to_country.py
@@ -0,0 +1,14 @@
+'''
+Standalone code that converts a city into a country
+When you run this code, a prompt will pop up asking you the name of the city.
+Need to install geopy on your machine
+'''
+
+from geopy.geocoders import Nominatim
+
+# convert city name into latitude and longitude
+city = input('Enter City Name: ') 
+geolocator = Nominatim(user_agent='marco')
+location = geolocator.geocode(city)
+country = location.address.split(',')[-1].strip()
+print(country)
