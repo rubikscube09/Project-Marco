9084009836bf8301a97f2ef195c90617fb5df50c
Shiyu Tian <shiyutian@cs.uchicago.edu>
2020-02-11 22:09:06 -0600

    Update get_safety

--- outline_pseudocode.py
+++ outline_pseudocode.py
@@ -1,3 +1,11 @@
+import re
+import util
+import bs4
+import queue
+import json
+import sys
+import csv
+
 #FUNCTIONS TO FILL DATAFRAME
 def get_destinations(starting_url,n):
     '''
@@ -20,10 +28,33 @@ def get_languages(df):
     Add language info to dataframe.
     '''
 
-def get_safety(df):
+def get_safety(starting_url):
     '''
-    Add safety info to dataframe
-    '''
+	Scrapes the travel advisory info on the State Department website and \
+	gives a list containing nation name and advisory level.
+
+	Input: a string of url
+	Output: list
+	'''
+	request = util.get_request(starting_url)
+	current_url = util.get_request_url(request)
+	html = util.read_request(request)
+	soup = bs4.BeautifulSoup(html, "html5lib")
+	table = soup.find_all("table")
+	tbody = table[0].find_all("tbody")
+	tr_list = table[0].find_all("tr")
+	travel_advisory = []
+	for tr in tr_list:
+		td = tr.find_all("td")
+		if td != [] and td[0].text != "Worldwide Caution":
+			td = td[ : -1]
+			list_td = td[0].text.split()
+			list_td = list_td[ : -2]
+			td[0] = " ".join(list_td)
+			td[1] = td[1].text
+			travel_advisory.append(td)
+			
+	return travel_advisory
 
 def get_flight_costs(df, travel_dates, starting_dest):
     '''
