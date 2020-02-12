6b581ab0bf5d6bc4c45995ef67d41247d37d3061
haozhu <haozhu@uchicago.edu>
2020-02-12 01:24:30 -0600

    Language data

---
+++ language.py
@@ -0,0 +1,23 @@
+'''
+Code for getting percentage of English speaker by country, outputs a json/
+dictionary. Could modify to include more information.
+'''
+
+import bs4
+import json
+
+filename = 'List countries English-speaking population.html'
+html = open(filename, encoding='cp437').read()
+soup = bs4.BeautifulSoup(html, "html5lib")
+table = soup.find_all("table", class_='wikitable sortable')
+body = table[0].find_all("tbody")
+tag_countries = body[0].find_all('tr')
+percent_dict = {}
+for tag_country in tag_countries[2:-1]:
+    data = tag_country.find_all('td')
+    country = data[0].find_all('a')[0].text
+    percentage = data[3].text.strip()
+    percent_dict[country] = percentage
+
+with open('language.json', 'w') as file:
+    json.dump(percent_dict, file)
