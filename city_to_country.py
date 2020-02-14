a3fbd556656c475ff4020c23a991a9f5ad624b3d
haozhu <haozhu@uchicago.edu>
2020-02-13 20:19:31 -0600

    Slight improvement

--- city_to_country.py
+++ city_to_country.py
@@ -5,10 +5,11 @@ Need to install geopy on your machine
 '''
 
 from geopy.geocoders import Nominatim
+import re
 
 # convert city name into latitude and longitude
 city = input('Enter City Name: ') 
 geolocator = Nominatim(user_agent='marco')
-location = geolocator.geocode(city)
+location = geolocator.geocode(city, language='en-US')
 country = location.address.split(',')[-1].strip()
 print(country)
