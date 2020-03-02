8a6ed3548d3ef5f15e7612077b88d91488b3800f
Abhimanyu Choudhary <achoudhary@cs.uchicago.edu>
2020-03-02 13:19:25 -0600

    trip advisor subtype keywords. Need to reorganize and filter these

---
+++ preprocessing/trip_advisor_consts.py
@@ -0,0 +1,23 @@
+BOAT_TOURS = 'Kayaking & canoeing,Stand-Up Paddleboarding,Waterskiing & jetskiing,Swim with dolphins,Surfing, Windsurfing & Kitesurfing,Submarine tours,Shark diving,Scuba & snorkelling,River rafting & tubing,Parasailing & paragliding,Water sports,Speed boats tours,Duck tours,Boat Tours,Boat rentals,Dolphin & whale watching,Fishing charters & tours,Gondola cruises'.split(',')
+
+CASINOS = ['Casinos']
+
+CONCERTS_SHOWS = 'Jazz Bars,Performances,Dinner Theaters,Concerts,Operas,Symphonies, Ballets,Theaters,Cabaret,Cirque du Soleil,Luaus,Sporting events & packages,Theater & musicals,Blues Bars,Piano Bar,Country Western Bars,Comedy Club'.split(',')
+
+FOOD_DRINK = 'Farmers markets,Distillery Tours,Wineries,Breweries,Distilleries,Beer tastings & tours,Coffee & tea tours,Cooking classes,Food tours,Wine tastings & tours,Wine Bar,Other food and wine'.split(',')
+
+MUSEUMS = 'Observatories/ Planetariums,Military Museums,Museums,Natural History Museums,Children\'s Museums,Science Museums,Specialty Museums,Art Galleries,Art Museums,History Museums'.split(',')
+
+NATURE_PARKS = 'National Parks,Biking Trails,Equestrian Trails,Hiking Trails,Motorcycle Trails,Off road/All Terrain Vehicle Trails,Ski/ Snowboard Areas,Beaches,Zoos,Aquariums,Bodies of Water,Canyons,Caverns/ Caves,Dams,Deserts,Forests,Gardens,Geologic Formations,Hot Springs/ Geysers,Islands,Marinas,Mountains,Nature/ Wildlife Areas,Parks,Reefs,State Parks,Valleys,Volcanos,Waterfalls,Other nature and parks'.split(',')
+
+OUTDOOR_ACTIVITIES = 'Sports Camps/ Clinics,Horseback Riding Stables,Golf Courses,Motorcycle Trails,Hiking Trails,Cross-country Ski Areas,Zoos,Surf Camps,Submarine tours,Parasailing & paragliding,Shark diving,Scuba & snorkelling,River rafting & tubing,Boat rentals,Dolphin & whale watching,Gondola cruises,Speed boats tours,Kayaking & canoeing,Equestrian Trails,Off road/All Terrain Vehicle Trails,Biking Trails,Ski/ Snowboard Areas,Trails,Beaches,Scenic Drives,Horse-Drawn Carriage Tours,Jogging Paths/ Tracks,Resort Communities,Other outdoor activities,Zipline tours,Stand-Up Paddleboarding,Ski & snow tours,Safaris, Running tours,Canyoning & Rappelling Tours,Air tours,Nature & wildlife tours,Boat Tours,Hiking & camping tours,Golf tours & tee times,Fishing charters & tours,Eco tours,Climbing tours,Bike tours,Balloon rides,Adrenaline & extreme tours,4WD, ATV & off-road tours,Duck tours,Waterskiing & jetskiing,Swim with dolphins,Surfing, Windsurfing & Kitesurfing,Gear rentals,Water sports'.split(',')
+
+SHOPPING = 'Malls,Shops,Specialty Shops,Art Galleries,Airport Shops,Shopping tours,Fashion shows & tours,Farmers markets,Department Stores,Factory Outlets,Antique Shops,Flea/ Street Markets'.split(',')
+
+SPAS_AND_WELLNESS = 'Roman baths,Arab baths,Day Spas,Hammams & Turkish baths,Yoga & pilates,Thermal spas & hot springs,Health Clubs,Spas,Onsen Resorts'.split(',')
+
+TOURS = 'Gondola cruises,Skydiving,Day Trips,Boat rentals,Dolphin & whale watching,Speed boats tours,Kayaking & canoeing,Parasailing & paragliding, River rafting & tubing,Scuba & snorkelling,Shark diving,Submarine tours,Scenic Railroads,Tours,Factory Tours,Walking tours,Bus tours,Segway Tours,Water sports,Surfing, Windsurfing & Kitesurfing,Swim with dolphins,Waterskiing & jetskiing,Duck tours,Beer tastings & tours,Coffee & tea tours,Food tours,Wine tastings & tours,4WD, ATV & off-road tours,Adrenaline & extreme tours,Balloon rides,Bike tours,Climbing tours,Eco tours,Fishing charters & tours,Golf tours & tee times,Hiking & camping tours,Nature & wildlife tours,Running tours,Air tours,Archaeology tours,City tours,Cultural tours,Ghost & vampire tours,Helicopter tours,Historical & heritage tours,Hop-on Hop-off tours,Literary, art & music tours,Motorcycle tours,Movie & TV tours,Night tours,Ports of Call tours,Private tours,Rail tours,Self-guided tours & rentals,Skip-the-Line tours,Vespa, scooter & moped tours,Bar, club & pub tours,Fashion shows & tours,Shopping tours,Horse-Drawn Carriage Tours,Photography Tours,Stand-Up Paddleboarding,Canyoning & Rappelling Tours,Distillery Tours,Boat Tours,Zipline tours'.split(',')
+
+AMUSEMENT_PARKS = 'Amusement/ Theme Parks,Disney,Water Parks'.split(',')
+
+ZOOS = ' Zoos,Aquariums,Other zoos & aquariums'.split(',')
