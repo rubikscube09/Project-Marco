5dc303a4a4b608906b283f36138511b0340a9829
haozhu <haozhu@uchicago.edu>
2020-03-02 18:21:01 -0600

    Scraped ~260 cities, debug for more

--- preprocessing/scraping/keyword alternative/twiki_city_links.py
+++ preprocessing/scraping/keyword alternative/twiki_city_links.py
@@ -44,7 +44,15 @@ def scrape(url):
 
     html = requests.get(url).text
     soup = bs4.BeautifulSoup(html, "html5lib")
-    section = soup.select('#See')[0].parent
+    try:
+        section = soup.select('#See')[0].parent
+    except:
+        redirect = soup.find_all('ul', class_='redirectText')[0]
+        redirect = redirect.find_all('li')[0].find_all('a')
+        redirect_link = 'https://wikitravel.org' + redirect[0]['href']
+        html = requests.get(redirect_link).text
+        soup = bs4.BeautifulSoup(html, 'html5lib')
+        section = soup.select('#See')[0].parent
     output = {}
     name = 'See'
     subcategory = 'Description'
@@ -55,6 +63,8 @@ def scrape(url):
         # move that into the while condition
         # NOTE: 1. work on removing the italicized stuff
         #       2. I am missing another deeper level here
+        #           so I am not scraping anything for some pages
+        # The city Arusha does not have tag SEE or DO, consider this
         if section.next_sibling != None:
             section = section.next_sibling
         else:
@@ -62,10 +72,10 @@ def scrape(url):
         if section.name == 'h2' or section.name == 'h3' or section.name == 'p':
             if section.name == 'h2':
                 section_title = re.findall(r'(?<!\[)\b\w+\b(?![\]])', section.text)
-                if section_title[0] == 'Buy':
+                if section_title[0][:3] == 'Buy' or section_title[0][:3] == 'Eat':
                     not_buy = False
                     break
-                elif section_title[0] == 'Do':
+                elif section_title[0][:3] == 'Do':
                     name = 'Do'
                     subcategory = 'Description'
                     subcat_dict = {}
@@ -73,16 +83,16 @@ def scrape(url):
                 subcategory = re.findall(r'(.*)\[', section.text)[0]
             elif section.name == 'p':
                 if subcategory not in subcat_dict:
-                    subcat_dict[subcategory] = section.text
+                    subcat_dict[subcategory] = section.text.strip()
                 else:
-                    subcat_dict[subcategory] += section.text
+                    subcat_dict[subcategory] += section.text.strip()
 
             output[name] = subcat_dict
             
     return output
 
 
-def scrape_all():
+def scrape_all(verbose=False):
     '''
     Find links and scrape everything.
     '''
@@ -95,7 +105,11 @@ def scrape_all():
     base_url = 'https://wikitravel.org/en/'
 
     city_data = {}
+    count = 0
     for city in cities.array:
+        if verbose:
+            count += 1
+            print('We have scraped {} cities'.format(count))
         print('Scraping', city)
         url = base_url + city
         html = requests.get(url).text
@@ -107,10 +121,9 @@ def scrape_all():
         empty_tag = soup.find_all('div', \
                                     class_ = 'noarticletext mw-content-ltr')
         # nothing found
-        # NOTE: need to correct Rio_De_Janeiro to Rio_de_Janeiro, don't know
-        # about others yet
+        # NOTE: need to correct Rio_De_Janeiro to Rio_de_Janeiro,
         if len(empty_tag) == 1:
-            print('Nothing to scrape, moving on...')
+            print('Nothing here, buds!')
             city_data[city] = 'Not available'
             continue
 
@@ -120,13 +133,22 @@ def scrape_all():
             print('Multiple possible matches, might take some time...')
             possible_cities = soup.find_all('ul')
             for tag in possible_cities:
-                if any(re.findall(r'city|town', tag.text)):
-                    # if the tag indeed leads to a city, get its url
-                    url = 'https://wikitravel.org/' + \
-                                                tag.find_all('a')[0]['href']
-                    city_name = re.findall(r'(.*)-', tag.text)[0]
-                    output = scrape(url)
-                    city_data[city_name] = output
+                list_cities = tag.find_all('li')
+                for city in list_cities:
+                # actually you should find list and then continue
+                    if any(re.findall(r'city|town', city.text)):
+                        # if the tag indeed leads to a city, get its url
+                        link_tag =  tag.find_all('a')[0]
+                        print(link_tag)
+                        if link_tag.get('class') != None and link_tag.get('class')[0] != 'mw-selflink':
+                            link = link_tag['href']
+                            if 'action=edit' not in link:
+                                # if the link is not a new page waiting to be edited
+                                url = 'https://wikitravel.org/' + link
+                                city_name = re.findall(r'^[^-]*[^ -]', tag.text)[0]
+                                print('Scraping', city_name)
+                                output = scrape(url)
+                                city_data[city_name] = output
         else:
             # direct scraping
             print('Scraping directly, nice')
@@ -134,3 +156,5 @@ def scrape_all():
 
         with open('city_data.json', 'w') as f:
             json.dump(city_data, f)
+
+#scrape_all(True)
