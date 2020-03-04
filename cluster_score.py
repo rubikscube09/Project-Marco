b02d2be9b3a5342df1484b1248b05fefa716ba85
Abhimanyu Choudhary <achoudhary@cs.uchicago.edu>
2020-03-03 19:31:36 -0600

    Scoring with constants

--- cluster_score.py
+++ cluster_score.py
@@ -1,26 +1,34 @@
 import pandas as pd
 
-
-def calc_score(clusters, cities):
+def calc_score(clusters, attractions_list):
     '''
     Given a list of clusters, calculate the frequency of the keywords in each
     city and output to a pandas dataframe.
     '''
-
     out = pd.DataFrame()
+    rpt = False
+    city_column = []
     for cluster in clusters:
+        print(cluster)
         column = []
         count = 0
-        for city in cities:
-            # unpack the list of lists into a flat list
-            flat_list = [item for sublist in city for item in sublist]
-            for item in flat_list:
-                if item in cluster:
-                    count += 1
+        for city in attractions_list:
+            count = 0
+            for i in range(0,len(attractions_list[city])):
+                for attraction in attractions_list[city][i]:
+                    if attraction in clusters[cluster]:
+                        count += (30 - 0.5*i)/30
             column.append(count)
+            if rpt == False:
+                city_column.append(city)
+        rpt = True
         out[cluster] = column
         # normalize
-        out[cluster] = out[cluster] / out[cluster].sum()
+        # out[cluster] = out[cluster] / out[cluster].sum()
+    print(city_column)
+    out['city'] = city_column
+    out.set_index(['city'])
+    return out 
     # the ordering of the output dataframe should be the same as the order of 
     # the cities
 
