22732bf892eadafce2b9bfee062af0c7d0d4aca1
Ezra Max <emax@cs.uchicago.edu>
2020-03-04 18:51:03 -0600

    Replace process_static_data.py

--- preprocessing/process_static_data.py
+++ preprocessing/process_static_data.py
@@ -1,19 +1,20 @@
 import pandas as pd
-import process_city_keywords
 import country
 import language
 import safety
 import get_trip_advisor_static
 
-#df=process_city_keywords.df
-df=pd.read_csv("/scraping/destinations.csv")
+df=pd.read_csv("scraping/destinations.csv")
 
 df['country']=df.apply(lambda row: country.get_country(row['city']),axis=1)
 df['language']=df.apply(lambda row: language.get_language(row['country']),axis=1)
 df['safety']=df.apply(lambda row: safety.get_safety(row['country']), axis=1)
 
-df['trip_advisor_id']=df.apply(lambda row: get_trip_advisor_static.get_location_id(row['city']))
-df['attractions']=df.apply(lambda row: get_trip_advisor_static.get_location_id(row['trip_advisor_id']))
+df['trip_advisor_id']=df.apply(lambda row: get_trip_advisor_static.get_location_id(row['city']), axis=1)
+df['attractions']=df.apply(lambda row: get_trip_advisor_static.get_attractions(row['trip_advisor_id']), axis=1)
 
+#LOOK FOR "NONE"
+#RUN THIS PART!!!
+df.drop_duplicates("cities")
 
-df.to_csv("/CMSC-12200-Group-Project/destinations_with_static_info.csv", header=True, index=False)
+df.to_csv("destinations_with_static_info.csv", header=True, index=False)
