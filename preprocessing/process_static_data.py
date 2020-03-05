c1456c2a95718308351fc70595baf27d70356794
Ezra Max <emax@cs.uchicago.edu>
2020-03-04 20:37:47 -0600

    Replace process_static_data.py

--- preprocessing/process_static_data.py
+++ preprocessing/process_static_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 import country
 import language
 import safety
+import _pickle as cPickle
 import get_trip_advisor_static
 
 df=pd.read_csv("scraping/destinations.csv")
@@ -13,8 +14,12 @@ df['safety']=df.apply(lambda row: safety.get_safety(row['country']), axis=1)
 df['trip_advisor_id']=df.apply(lambda row: get_trip_advisor_static.get_location_id(row['city']), axis=1)
 df['attractions']=df.apply(lambda row: get_trip_advisor_static.get_attractions(row['trip_advisor_id']), axis=1)
 
-#LOOK FOR "NONE"
-#RUN THIS PART!!!
+
 df.drop_duplicates("cities")
 
-df.to_csv("destinations_with_static_info.csv", header=True, index=False)
+
+with open("trip_advisor_data.pkl", "wb") as f:
+    cPickle.dump(list(df['attractions']), f)
+
+del df['attractions']
+
