56723fd72811149f760a680a181c1d8d8cfb0540
Abhimanyu Choudhary <achoudhary@cs.uchicago.edu>
2020-03-07 14:04:11 -0600

    updated stuff

--- vacation_id3.py
+++ vacation_id3.py
@@ -6,29 +6,38 @@ from sklearn import metrics
 from sklearn.tree import export_graphviz
 from sklearn.externals.six import StringIO  
 from IPython.display import Image  
+from scipy import sparse
 import pydotplus
 
-
-
-scores = pd.read_csv('hundred_cities')
+scores = pd.read_csv('full_country_score')
+#scores.drop(['CASINOS'],axis= 1)
+all_scores = pd.DataFrame().append([scores]*200)
 feature_cols = scores.columns[1:] 
-diff_scores = [scores]*20
-all_scores = scores.append(diff_scores)
-variance = pd.DataFrame([scores.var(axis = 0)]*len(all_scores))
+scores[feature_cols] = np.sqrt(scores[feature_cols])
+#diff_scores = [scores]*100
+#all_scores = scores.append(diff_scores)
+variance = pd.DataFrame([scores.var(axis = 0)])
 for columns in feature_cols:
-	all_scores[columns] += np.random.normal(0,np.sqrt(variance[columns]/),len(all_scores))
+	all_scores[columns] += np.random.normal(0,np.sqrt(variance[columns]/2),len(all_scores))
+
 
 X = all_scores[feature_cols] 
+
 y = all_scores['city']
 
-X_train , X_test , y_train, y_test = train_test_split(X,y,test_size=0.1,random_state = 1)
 
-clf = DecisionTreeClassifier(max_depth = 5,criterion='entropy')
+X_train , X_test , y_train, y_test = train_test_split(X,y,test_size=0.3,random_state = 1)
+
+clf = DecisionTreeClassifier(max_depth = 10,criterion='entropy',min_samples_leaf = 5)
 clf = clf.fit(X_train,y_train)
+
 
 y_pred = clf.predict(X_test)
 
+
 print('Accuracy:' + str(metrics.accuracy_score(y_test,y_pred)))
+print(clf.tree_.max_depth)
+
 dot_data = StringIO()
 export_graphviz(clf, out_file=dot_data,  
                 filled=True, rounded=True,
@@ -36,5 +45,3 @@ export_graphviz(clf, out_file=dot_data,
 graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
 graph.write_png('vacations.png')
 Image(graph.create_png())
-
-
