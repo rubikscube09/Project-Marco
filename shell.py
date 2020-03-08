82b7ad8b1fa6432ca744a5a770087a02a37724a4
Ezra Max <emax@cs.uchicago.edu>
2020-03-08 13:12:38 -0500

    Update shell.py

--- shell.py
+++ shell.py
@@ -1,7 +1,11 @@
+import current_location
 import vacation_id3
 import pandas as pd
 import hotels
+from datetime import datetime, timedelta
 
+
+current_loc=current_location.get_location()
 df=pd.read_csv('preprocessing/destinations_with_static_info.csv')
 
 del df['Unnamed: 0']
@@ -13,9 +17,10 @@ def query(k,n, df=df):
     n: cities to return
     '''
     num_adults=input("Number of travelers: ")
-    check_in=input("Travel date: ")
-    nights=input("Nights: ")
-    
+    departure_date=input("Departure date: ")
+    return_date=input("Return date: ")
+    nights=difference =(return_date - departure_date).days
+    check_in=departure_date #NOT ALWAYS TRUE
     cities=vacation_id3.traverse(k)
 
     safety_weight=int(input('How important is safety? '))
@@ -29,7 +34,17 @@ def query(k,n, df=df):
     #DO FLIGHT DATA
     
     df['hotels']=df.apply(lambda row: hotels.get_hotels(row['trip_advisor_id'], num_adults, check_in, nights), axis=1)
-    df['best_price']=df.apply(lambda row: row['hotels'][0][1], axis=1)
+    df['flights']=ddf.apply(lambda row: kiwi.get_flights(current_loc, row['city'], 
+                departure_date, return_date, 
+                roundtrip = True,
+                return_from=row['city'], return_to=current_loc, 
+                adults=num_adults, children=0, infants=0,
+                budget=5000, currency='USD',
+                people=0, 
+                max_duration=50, 
+                radius=50, radius_format= 'km'), axis=1)
     
-    return df[['city','country','best_price', 'hotels','text','image']].sort_values('best_price', ascending=False)
+    df['best_price']=df.apply((lambda row: row['hotels'][0][1]+row['flights'][0]['price']), axis=1)
+    
+    return df[['city','country','best_price', 'hotels','flights','text','image']].sort_values('best_price', ascending=False)
 
