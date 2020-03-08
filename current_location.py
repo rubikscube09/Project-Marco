207d3a899ba3462861a864906b50a6c2dae2a7f0
Ezra Max <emax@cs.uchicago.edu>
2020-03-08 13:05:52 -0500

    Update current_location.py

--- current_location.py
+++ current_location.py
@@ -5,32 +5,33 @@ import requests
 # ip_address = ip_response.text
 import socket
 
-# the follwing part tests your internet connection
-connection = False
-try:
-    # see if we can resolve the host name -- tells us if 
-    # there is a DNS listening
-    host = socket.gethostbyname("www.google.com")
-    # connect to the host -- tells us if the host is actually reachable
-    s = socket.create_connection((host, 80), 2)
-    s.close()
-    connection = True
-except:
-    print('Please check your internet connection')
-
-# alternative API, unlimited calls, but this level of convenience makes me
-# question the legitimacy of the website
-if connection == True:
-    ip_response = requests.get('https://ifconfig.me/')
-    ip_address = ip_response.text
-
-    # using an api from ipstack.com
-    # the string after ? is my api access token
-
-    loc_request_url = 'http://api.ipstack.com/' + ip_address
-    location_response = requests.get(loc_request_url + \
-                            '?access_key=e24ee45fcafb4d395d01679458ac43dd')
-    location_json = location_response.json()
-    city = location_json['city']
-    lat = location_json['latitude']
-    lon = location_json['longitude']
+def get_location():
+    # the follwing part tests your internet connection
+    connection = False
+    try:
+        # see if we can resolve the host name -- tells us if 
+        # there is a DNS listening
+        host = socket.gethostbyname("www.google.com")
+        # connect to the host -- tells us if the host is actually reachable
+        s = socket.create_connection((host, 80), 2)
+        s.close()
+        connection = True
+    except:
+        input('Please enter location:' )
+    
+    # alternative API, unlimited calls, but this level of convenience makes me
+    # question the legitimacy of the website
+    if connection == True:
+        ip_response = requests.get('https://ifconfig.me/')
+        ip_address = ip_response.text
+    
+        # using an api from ipstack.com
+        # the string after ? is my api access token
+    
+        loc_request_url = 'http://api.ipstack.com/' + ip_address
+        location_response = requests.get(loc_request_url + \
+                                '?access_key=e24ee45fcafb4d395d01679458ac43dd')
+        location_json = location_response.json()
+        city = location_json['city']
+        lat = location_json['latitude']
+        lon = location_json['longitude']
