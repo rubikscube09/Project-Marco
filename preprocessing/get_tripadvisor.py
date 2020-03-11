e09545974ef1ee8e26fd266c8e707d3c1e7b23a9
Shiyu Tian <shiyutian@cs.uchicago.edu>
2020-03-11 18:07:08 -0500

    cleaned three files

--- preprocessing/get_tripadvisor.py
+++ preprocessing/get_tripadvisor.py
@@ -1,25 +1,19 @@
 import pandas as pd
 import trip_advisor_consts
 import hotels
+import get_trip_advisor_static
 clusters = trip_advisor_consts.CLUSTERS
 
 
 def get_attr_dict():
-	destinations = pd.read_csv('preprocessing/destinations_with_static_info.csv')
+	destinations = pd.read_csv('destinations_with_static_info.csv')
 	print(destinations)
 	sample_dests = list(zip(list(destinations['city']),list(destinations['trip_advisor_id'])))
 	attractions_dict = {}
 	name_desc_img = {}
 
 	for city,t_id in sample_dests:
-		data = hotels.get_attractions(t_id)
+		data = get_trip_advisor_static.get_attractions(t_id)
 		name_desc_img[city] = data[0]
 		attractions_dict[city] = data[1]
 	return [name_desc_img,attractions_dict]
-
-
-
-
-				
-
-	
